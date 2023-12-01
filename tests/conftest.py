85a068e2f5342cf7ada20b2961a3caa12a024b69
antonio <a.cavallo@cavallinux.eu>
2023-12-01 21:11:47 +0100

    fix mypy/ruff issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,47 +1,6 @@
-import io
-import functools
 from pathlib import Path
-from unittest import mock
 
 import pytest
-
-@pytest.fixture(scope="function")
-def help_wrapper():
-    def _helper(fn):
-        @functools.wraps(fn)
-        def _fn(*args, **kwargs):
-            class AbortHelp(Exception):
-                pass
-
-            try:
-                buf = io.StringIO()
-                name = getattr(fn, "__name__", fn.__class__.__name__)
-
-                def callme(self, parser, namespace, values, option_string=None):
-                    old_prog = parser.prog
-                    internal = {
-                        "_jb_pytest_runner.py",
-                        "py.test",
-                        "pytest",
-                    }
-                    if parser.prog in internal:
-                        parser.prog = name
-                    parser.print_help(buf)
-                    parser.prog = old_prog
-                    raise AbortHelp
-                with mock.patch("argparse._HelpAction.__call__", new=callme):
-                    fn(*args, **kwargs)
-                    raise RuntimeError(f"{name} didn't call --help")
-            except AbortHelp:
-                return (
-                    buf
-                    .getvalue()
-                    # py 3.7 -> 3.8 change
-                    .replace("optional arguments:", "options:")
-                )
-        return _fn
-    return _helper
-
 
 @pytest.fixture(scope="function")
 def test_tree():
