0cf5ae5eeba85c436b1a594e27fce2c40f3c358c
antonio <a.cavallo@cavallinux.eu>
2023-12-31 23:28:06 +0100

    fix mypy issues

--- src/pytest_tdd/tools.py
+++ src/pytest_tdd/tools.py
@@ -5,5 +5,5 @@ def generate_fs_tree(destdir: Path, txt: str):
     from pytest_tdd.tree import parse
 
     tree = parse(txt)
-    tree.root.name = str(destdir.expanduser().resolve()) + "/"
-    tree.generate(dryrun=False)
+    tree.root.name = str(destdir.expanduser().resolve()) + "/"  # type: ignore
+    tree.generate(dryrun=False)  # type: ignore
