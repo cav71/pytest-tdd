d4ba6a9d26e9aec3f43c4c5375b38a7e7335368c
antonio <a.cavallo@cavallinux.eu>
2024-01-02 23:34:31 +0100

    cleanup

--- src/pytest_tdd/tools.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def generate_fs_tree(destdir: Path, txt: str):
-    from pytest_tdd.tree import parse
-
-    tree = parse(txt)
-    tree.root.name = str(destdir.expanduser().resolve()) + "/"  # type: ignore
-    tree.generate(dryrun=False)  # type: ignore
