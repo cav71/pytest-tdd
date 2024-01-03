950a5b2799b04a65a40726d79908c78f732c65ff
antonio <a.cavallo@cavallinux.eu>
2024-01-03 09:06:05 +0100

    increase tests on tdd module

--- tests/test_tdd.py
+++ tests/test_tdd.py
@@ -2,13 +2,69 @@ import os
 import functools
 import json
 from pathlib import Path
+import textwrap
 
 from pytest_tdd import tdd
 import xml.etree.ElementTree as ET
 
 
+def _project_simple(mktree):
+    workdir = mktree(
+        """
+    ├── src/
+    │   └── package/
+    │       ├── __init__.py
+    │       └── subpackage/
+    │           ├── __init__.py
+    │           └── mod.py
+    └── tests/
+        ├── package/
+        │   ├── subpackage/
+        │   │   └── test_mod.py
+        │   └── test_mod.py
+        ├── subpackage/
+        │   └── test_mod.py
+        └── test_mod.py
+    """
+    )
+
+    src = workdir / "src" / "package" / "subpackage" / "mod.py"
+    src.write_text(
+        textwrap.dedent(
+            """
+    def func1(val):
+        return val*2
+
+    def func2(val):
+        return val*2
+    """
+        )
+    )
+
+    tst = workdir / "tests" / "package" / "subpackage" / "test_mode.py"
+    tst.write_text(
+        textwrap.dedent(
+            """
+    import pytest
+    from package.subpackage import mod
+
+    @pytest.mark.parametrize("val,expected", [ 
+        (1, 2), 
+        (2, 4),
+        (3, 99),
+        (4, 8),
+    ])
+    def test_func1(val, expected):
+        assert mod.func1(val) == expected
+    """
+        )
+    )
+    return workdir
+
+
 def test_lookup(mktree):
-    rootdir = mktree("""
+    rootdir = mktree(
+        """
 ├── package2/
 │   ├── __init__.py
 │   ├── modF.py
@@ -41,7 +97,8 @@ def test_lookup(mktree):
     │   └── test_modG.py
     ├── test_modD.py
     └── test_modG.py
-""")
+"""
+    )
 
     sources_dir = rootdir / "src"
     tests_dir = rootdir / "tests"
@@ -61,71 +118,30 @@ def test_lookup(mktree):
 
 
 def test_run_simple(mktree):
-    workdir = mktree("""
-├── src/
-│   └── package/
-│       ├── __init__.py
-│       └── subpackage/
-│           ├── __init__.py
-│           └── mod.py
-└── tests/
-    ├── package/
-    │   ├── subpackage/
-    │   │   └── test_mod.py
-    │   └── test_mod.py
-    ├── subpackage/
-    │   └── test_mod.py
-    └── test_mod.py
-""")
-
-    src = workdir / "src" / "package" / "subpackage" / "mod.py"
-    src.write_text("""
-def func1(val):
-    return val*2
-    
-def func2(val):
-    return val*2
-""")
-
+    workdir = _project_simple(mktree)
     candidates = [
         workdir / "tests" / "package" / "subpackage" / "test_mode.py",
     ]
 
-    candidates[0].write_text("""
-import pytest
-from package.subpackage import mod
-
-@pytest.mark.parametrize("val,expected", [ 
-    (1, 2), 
-    (2, 4),
-    (3, 99),
-    (4, 8),
-])
-def test_func1(val, expected):
-    assert mod.func1(val) == expected
-""")
-    ret, out = tdd.run(
-        workdir,
-        "package",
-        candidates,
-        workdir / "src"
-    )
+    ret, out = tdd.run(workdir, "package", candidates, workdir / "src")
 
     assert ret == 1
 
     coverage = json.loads(out["coverage"])
     assert len(coverage["files"]) == 3
     assert (
-            coverage["files"][str(Path("src/package/subpackage/mod.py"))]
-            ["summary"]["percent_covered"] == 75.0
+        coverage["files"][str(Path("src/package/subpackage/mod.py"))]["summary"][
+            "percent_covered"
+        ]
+        == 75.0
     )
     assert coverage["totals"]["percent_covered"] == 75.0
 
     tests = ET.fromstring(out["tests"])
     assert len(tests) == 1
-    assert tests[0].attrib["failures"] == '1'
-    assert tests[0].attrib["tests"] == '4'
-    assert tests[0].attrib["skipped"] == '0'
+    assert tests[0].attrib["failures"] == "1"
+    assert tests[0].attrib["tests"] == "4"
+    assert tests[0].attrib["skipped"] == "0"
 
 
 def test_process(resolver):
@@ -134,4 +150,9 @@ def test_process(resolver):
         "coverage": resolver.resolve("coverage.json").read_text(),
         "tests": resolver.resolve("junit.xml").read_text(),
     }
-    assert result
+
+    assert (
+        tdd.process(Path("a/b/file-name"), result)
+        == "file-name run 4 tests with 1 failures and 0 errors, covered 3 lines out "
+        "of 4 (75.0%, missing=1 lines)"
+    )
